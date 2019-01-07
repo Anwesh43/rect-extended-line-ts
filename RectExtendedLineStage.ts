9cde80bb36f6f37eb9d86351076f33c2c07b67b4
Anwesh43 <anweshthecool0@gmail.com>
2019-01-07 09:40:43 +0530

    adding scale methods

--- RectExtendedLineStage.ts
+++ RectExtendedLineStage.ts
@@ -9,6 +9,17 @@ const foreColor : string = "#1565C0"
 const backColor : string = "#BDBDBD"
 const delay = 40
 
+const maxScale : Function = (scale : number, i : number, n : number) => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) => Math.min(1/n, maxScale(scale, i, n)) * n
+const scaleFactor : Function = (scale : number) => Math.floor(scale / scDiv)
+const mirrorValue : Function = (scale : number, a : number, b : number) => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class RectExtendedLineStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
